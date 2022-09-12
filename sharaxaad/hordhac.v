c908a24a6bcc1c908e41f2ca562c089adef5a88c
mohamedabdiahmed <mohamedahmedabdiah@gmail.com>
2022-09-12 17:57:54 +0300

    waxaan kusoo daray faahfaahin dheeri ah

---
+++ sharaxaad/hordhac.v
@@ -0,0 +1,76 @@
+waa maxay database 
+siduu u shaqeeyaa
+yaa isticmaala database
+ujeedada guud ee database laga leeyahay 
+database la,aanti qofku wax ma dhisi karaa
+database ku ma programming language ka ayuu kamid yahay 
+database ka keebaa i anfacaya barashadiisa 
+
+# waa maxay database 
+database waa meel aynu ku habaysanayno xogeheena  waxa uu inoo kaydiyaa xog kaas oo aynu computer ka u adeegsano in aynu xogteeni wax kaga badali karno waxna kaga dari karno 
+shaykasta oo aynu isticmaalno wuxu leyahay qayb wax kaydisa oo loo yaqaan database;
+
+
+# siduu u shaqeeyaa :
+wuxu u shaqeeyaa qaab electronic ahaan ah 
+halka lagu maamulona ay tahay computer wixii kale ee surto gal ah 
+
+
+# yaa isticmaala database:
+waxa isticmala qofkasta oo adeegsanaya technology ga 
+sidoo kalena ah qof xogo badan ururinaya sida shirkad dawlad iyo shaqsi caadi ahba
+
+ # ujeedada database laga leeyahay:
+ waa in aynu ku kaydisan karno xogeheena gaarka ah iyo kuwo 
+ guud ba
+
+ # database la,aanti qofku wax ma dhisi karaa:
+
+database waa halka go,aamisa systemkeena ama software keena 
+maxaa yeley ujeedada aynu database u isticmalayno ayaa ah in uu inoo kaydiyo xogta aynu rabno 
+
+#database ku ma programming ka ayuu kami yahay:
+
+database ku waa meesha aynu xogta ku kayd sanayno 
+hadaba waxa jira tools inaga caawinaya in aynu database ka la xidhiidhno 
+kuwaas oo ah qayb isaga u gaar ah oo loo yaqaan query langauge 
+waana ay faro badan yihin 
+
+waxa kamid ah :
+1. sql server 
+2. postgresql 
+3. mysql 
+4. mongodb 
+
+
+
+#database ka barashadiisa keebaa i anfacaya:
+
+ugu horeyn waxaynu isla eegeyna imisa database ayaa jira 
+aduunku isticmala
+haa wa waydiin mudan 
+1. nooc looyaqaan RDBM (Relational Database Management System);
+2. NoSQL
+hadaba labada database aya waxay ku kala duwan yihiin 
+in ka hore uu leeyahay wax loo yaqaan tables sidoo kale columns iyo rows waxa usii dheer in uu yahay mid ay tables kiisu isku xidhmayaan 
+
+nooca labaad ayaa ah mid isna database ah balse 
+habka uu xogta u kaydiyo ayaa ah in uu isticmalo 
+document sidoo kale key-value iyo type-value 
+manalaha wax isku xidh ah 
+
+hadaba barasho ahaan kee ayaa i anfacaya ama aan horeysiya
+barasho ahaan waxa horeysiin doonta insha allah 
+# sql server 
+# postgresql
+# mysql
+kii aad doonto 
+wana isku mid oo hal ujeedo ayay u wada socdaan oo ah inay xog kuu kaydinayaan 
+waxaad maanka ku haysa databases yada aynu shegnay ay yihiin 
+luuqadaha lagula xidhiidhayo database ka 
+
+
+hadaba halkan waxaan kusoo gudbin doonaa 
+nooc kamid ah database kaas oo loo yaqaan postgresql.
+
+
---
+++ sharaxaad/postgresql.v
@@ -0,0 +1,16 @@
+postgresql waa nooc kamid ah databases ka la isticmalo 
+kuwooda aduunka ugu horeya waqti xaadirkan 
+hadaba muxu kaga duwan yahay databases ka kale 
+
+1. waa open-source oo ah mid furan oo cidkasta isticmaali karto waxna ku darsan karto 
+halka databases ka qaarkood aanad waxba ku darsan karin oo ay shirkado leyihiin 
+sidoo kalena ay maamulaan 
+balse kan aduunka oo dhan ayaa uu ka dhaxeeyaa waana mid free ah 
+2. soo dejintiisu waa mid aad u fudud sidoo kale ku shubistiisu waa mid fudud
+waxay ku xidhan tahay operatem system ka aad isticmaalayso 
+sidoo kale waana crose-platform oo aan ula jeedno waa mid 
+la shaqayn kara operating system kasta sida windows linux mac 
+3. barasho ahaan waa mid aad u fudud sidoo kale waa databases ka loo yaqaan RDBM(Relational Database Management System)
+4. waxa lagu dhisi kara database yada webapplications ka iyo mobile applciation 
+kuwaas oo uu awood xoogan ku leeyahay 
+5. waxyaabo uu kaga soocan yahay database yada kale ayaa inoo iman doona 
